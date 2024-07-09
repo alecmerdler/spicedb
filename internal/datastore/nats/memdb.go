6a4090caec6ff4bc88c0f8ed5c80edeb17c458f1
Alec Merdler <alecmerdler@gmail.com>
2024-07-09 17:15:47 -0400

    copied memdb datastore into nats to start with a full implementation and modify incrementally

---
+++ internal/datastore/nats/memdb.go
@@ -0,0 +1,367 @@
+package memdb
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/authzed/spicedb/internal/datastore/common"
+	"github.com/authzed/spicedb/pkg/spiceerrors"
+	"github.com/authzed/spicedb/pkg/tuple"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/go-memdb"
+
+	"github.com/authzed/spicedb/internal/datastore/revisions"
+	"github.com/authzed/spicedb/pkg/datastore"
+	"github.com/authzed/spicedb/pkg/datastore/options"
+	corev1 "github.com/authzed/spicedb/pkg/proto/core/v1"
+)
+
+const (
+	Engine                   = "nats"
+	defaultWatchBufferLength = 128
+	numAttempts              = 10
+)
+
+var ErrSerialization = errors.New("serialization error")
+
+// DisableGC is a convenient constant for setting the garbage collection
+// interval high enough that it will never run.
+const DisableGC = time.Duration(math.MaxInt64)
+
+// NewNatsDatastore creates a new Datastore compliant datastore backed by memdb.
+//
+// If the watchBufferLength value of 0 is set then a default value of 128 will be used.
+func NewNatsDatastore(
+	watchBufferLength uint16,
+	revisionQuantization,
+	gcWindow time.Duration,
+) (datastore.Datastore, error) {
+	if revisionQuantization > gcWindow {
+		return nil, errors.New("gc window must be larger than quantization interval")
+	}
+
+	if revisionQuantization <= 1 {
+		revisionQuantization = 1
+	}
+
+	db, err := memdb.NewMemDB(schema)
+	if err != nil {
+		return nil, err
+	}
+
+	if watchBufferLength == 0 {
+		watchBufferLength = defaultWatchBufferLength
+	}
+
+	uniqueID := uuid.NewString()
+	return &natsDatastore{
+		CommonDecoder: revisions.CommonDecoder{
+			Kind: revisions.Timestamp,
+		},
+		db: db,
+		revisions: []snapshot{
+			{
+				revision: nowRevision(),
+				db:       db,
+			},
+		},
+
+		negativeGCWindow:        gcWindow.Nanoseconds() * -1,
+		quantizationPeriod:      revisionQuantization.Nanoseconds(),
+		watchBufferLength:       watchBufferLength,
+		watchBufferWriteTimeout: 100 * time.Millisecond,
+		uniqueID:                uniqueID,
+	}, nil
+}
+
+type natsDatastore struct {
+	sync.RWMutex
+	revisions.CommonDecoder
+
+	db             *memdb.MemDB
+	revisions      []snapshot
+	activeWriteTxn *memdb.Txn
+
+	negativeGCWindow        int64
+	quantizationPeriod      int64
+	watchBufferLength       uint16
+	watchBufferWriteTimeout time.Duration
+	uniqueID                string
+}
+
+type snapshot struct {
+	revision revisions.TimestampRevision
+	db       *memdb.MemDB
+}
+
+func (mdb *natsDatastore) SnapshotReader(dr datastore.Revision) datastore.Reader {
+	mdb.RLock()
+	defer mdb.RUnlock()
+
+	if len(mdb.revisions) == 0 {
+		return &natsReader{nil, nil, fmt.Errorf("memdb datastore is not ready"), time.Now()}
+	}
+
+	if err := mdb.checkRevisionLocalCallerMustLock(dr); err != nil {
+		return &natsReader{nil, nil, err, time.Now()}
+	}
+
+	revIndex := sort.Search(len(mdb.revisions), func(i int) bool {
+		return mdb.revisions[i].revision.GreaterThan(dr) || mdb.revisions[i].revision.Equal(dr)
+	})
+
+	// handle the case when there is no revision snapshot newer than the requested revision
+	if revIndex == len(mdb.revisions) {
+		revIndex = len(mdb.revisions) - 1
+	}
+
+	rev := mdb.revisions[revIndex]
+	if rev.db == nil {
+		return &natsReader{nil, nil, fmt.Errorf("memdb datastore is already closed"), time.Now()}
+	}
+
+	roTxn := rev.db.Txn(false)
+	txSrc := func() (*memdb.Txn, error) {
+		return roTxn, nil
+	}
+
+	return &natsReader{noopTryLocker{}, txSrc, nil, time.Now()}
+}
+
+func (mdb *natsDatastore) SupportsIntegrity() bool {
+	return true
+}
+
+func (mdb *natsDatastore) ReadWriteTx(
+	ctx context.Context,
+	f datastore.TxUserFunc,
+	opts ...options.RWTOptionsOption,
+) (datastore.Revision, error) {
+	config := options.NewRWTOptionsWithOptions(opts...)
+	txNumAttempts := numAttempts
+	if config.DisableRetries {
+		txNumAttempts = 1
+	}
+
+	for i := 0; i < txNumAttempts; i++ {
+		var tx *memdb.Txn
+		createTxOnce := sync.Once{}
+		txSrc := func() (*memdb.Txn, error) {
+			var err error
+			createTxOnce.Do(func() {
+				mdb.Lock()
+				defer mdb.Unlock()
+
+				if mdb.activeWriteTxn != nil {
+					err = ErrSerialization
+					return
+				}
+
+				if mdb.db == nil {
+					err = fmt.Errorf("datastore is closed")
+					return
+				}
+
+				tx = mdb.db.Txn(true)
+				tx.TrackChanges()
+				mdb.activeWriteTxn = tx
+			})
+
+			return tx, err
+		}
+
+		newRevision := mdb.newRevisionID()
+		rwt := &natsReadWriteTx{natsReader{&sync.Mutex{}, txSrc, nil, time.Now()}, newRevision}
+		if err := f(ctx, rwt); err != nil {
+			mdb.Lock()
+			if tx != nil {
+				tx.Abort()
+				mdb.activeWriteTxn = nil
+			}
+
+			// If the error was a serialization error, retry the transaction
+			if errors.Is(err, ErrSerialization) {
+				mdb.Unlock()
+
+				// If we don't sleep here, we run out of retries instantaneously
+				time.Sleep(1 * time.Millisecond)
+				continue
+			}
+			defer mdb.Unlock()
+
+			// We *must* return the inner error unmodified in case it's not an error type
+			// that supports unwrapping (e.g. gRPC errors)
+			return datastore.NoRevision, err
+		}
+
+		mdb.Lock()
+		defer mdb.Unlock()
+
+		tracked := common.NewChanges(revisions.TimestampIDKeyFunc, datastore.WatchRelationships|datastore.WatchSchema, 0)
+		if tx != nil {
+			if config.Metadata != nil && len(config.Metadata.GetFields()) > 0 {
+				if err := tracked.SetRevisionMetadata(ctx, newRevision, config.Metadata.AsMap()); err != nil {
+					return datastore.NoRevision, err
+				}
+			}
+
+			for _, change := range tx.Changes() {
+				switch change.Table {
+				case tableRelationship:
+					if change.After != nil {
+						rt, err := change.After.(*relationship).Relationship()
+						if err != nil {
+							return datastore.NoRevision, err
+						}
+
+						if err := tracked.AddRelationshipChange(ctx, newRevision, rt, tuple.UpdateOperationTouch); err != nil {
+							return datastore.NoRevision, err
+						}
+					} else if change.After == nil && change.Before != nil {
+						rt, err := change.Before.(*relationship).Relationship()
+						if err != nil {
+							return datastore.NoRevision, err
+						}
+
+						if err := tracked.AddRelationshipChange(ctx, newRevision, rt, tuple.UpdateOperationDelete); err != nil {
+							return datastore.NoRevision, err
+						}
+					} else {
+						return datastore.NoRevision, spiceerrors.MustBugf("unexpected relationship change")
+					}
+				case tableNamespace:
+					if change.After != nil {
+						loaded := &corev1.NamespaceDefinition{}
+						if err := loaded.UnmarshalVT(change.After.(*namespace).configBytes); err != nil {
+							return datastore.NoRevision, err
+						}
+
+						err := tracked.AddChangedDefinition(ctx, newRevision, loaded)
+						if err != nil {
+							return datastore.NoRevision, err
+						}
+					} else if change.After == nil && change.Before != nil {
+						err := tracked.AddDeletedNamespace(ctx, newRevision, change.Before.(*namespace).name)
+						if err != nil {
+							return datastore.NoRevision, err
+						}
+					} else {
+						return datastore.NoRevision, spiceerrors.MustBugf("unexpected namespace change")
+					}
+				case tableCaveats:
+					if change.After != nil {
+						loaded := &corev1.CaveatDefinition{}
+						if err := loaded.UnmarshalVT(change.After.(*caveat).definition); err != nil {
+							return datastore.NoRevision, err
+						}
+
+						err := tracked.AddChangedDefinition(ctx, newRevision, loaded)
+						if err != nil {
+							return datastore.NoRevision, err
+						}
+					} else if change.After == nil && change.Before != nil {
+						err := tracked.AddDeletedCaveat(ctx, newRevision, change.Before.(*caveat).name)
+						if err != nil {
+							return datastore.NoRevision, err
+						}
+					} else {
+						return datastore.NoRevision, spiceerrors.MustBugf("unexpected namespace change")
+					}
+				}
+			}
+
+			var rc datastore.RevisionChanges
+			changes, err := tracked.AsRevisionChanges(revisions.TimestampIDKeyLessThanFunc)
+			if err != nil {
+				return datastore.NoRevision, err
+			}
+
+			if len(changes) > 1 {
+				return datastore.NoRevision, spiceerrors.MustBugf("unexpected MemDB transaction with multiple revision changes")
+			} else if len(changes) == 1 {
+				rc = changes[0]
+			}
+
+			change := &changelog{
+				revisionNanos: newRevision.TimestampNanoSec(),
+				changes:       rc,
+			}
+			if err := tx.Insert(tableChangelog, change); err != nil {
+				return datastore.NoRevision, fmt.Errorf("error writing changelog: %w", err)
+			}
+
+			tx.Commit()
+		}
+		mdb.activeWriteTxn = nil
+
+		// Create a snapshot and add it to the revisions slice
+		if mdb.db == nil {
+			return datastore.NoRevision, fmt.Errorf("datastore has been closed")
+		}
+
+		snap := mdb.db.Snapshot()
+		mdb.revisions = append(mdb.revisions, snapshot{newRevision, snap})
+		return newRevision, nil
+	}
+
+	return datastore.NoRevision, NewSerializationMaxRetriesReachedErr(errors.New("serialization max retries exceeded; please reduce your parallel writes"))
+}
+
+func (mdb *natsDatastore) ReadyState(_ context.Context) (datastore.ReadyState, error) {
+	mdb.RLock()
+	defer mdb.RUnlock()
+
+	return datastore.ReadyState{
+		Message: "missing expected initial revision",
+		IsReady: len(mdb.revisions) > 0,
+	}, nil
+}
+
+func (mdb *natsDatastore) OfflineFeatures() (*datastore.Features, error) {
+	return &datastore.Features{
+		Watch: datastore.Feature{
+			Status: datastore.FeatureSupported,
+		},
+		IntegrityData: datastore.Feature{
+			Status: datastore.FeatureSupported,
+		},
+		ContinuousCheckpointing: datastore.Feature{
+			Status: datastore.FeatureUnsupported,
+		},
+		WatchEmitsImmediately: datastore.Feature{
+			Status: datastore.FeatureUnsupported,
+		},
+	}, nil
+}
+
+func (mdb *natsDatastore) Features(_ context.Context) (*datastore.Features, error) {
+	return mdb.OfflineFeatures()
+}
+
+func (mdb *natsDatastore) Close() error {
+	mdb.Lock()
+	defer mdb.Unlock()
+
+	if db := mdb.db; db != nil {
+		mdb.revisions = []snapshot{
+			{
+				revision: nowRevision(),
+				db:       db,
+			},
+		}
+	} else {
+		mdb.revisions = []snapshot{}
+	}
+
+	mdb.db = nil
+
+	return nil
+}
+
+var _ datastore.Datastore = &natsDatastore{}
---
+++ internal/datastore/nats/readwrite.go
@@ -0,0 +1,386 @@
+package memdb
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	v1 "github.com/authzed/authzed-go/proto/authzed/api/v1"
+	"github.com/hashicorp/go-memdb"
+	"github.com/jzelinskie/stringz"
+
+	"github.com/authzed/spicedb/internal/datastore/common"
+	"github.com/authzed/spicedb/pkg/datastore"
+	"github.com/authzed/spicedb/pkg/datastore/options"
+	core "github.com/authzed/spicedb/pkg/proto/core/v1"
+	"github.com/authzed/spicedb/pkg/spiceerrors"
+	"github.com/authzed/spicedb/pkg/tuple"
+)
+
+type natsReadWriteTx struct {
+	natsReader
+	newRevision datastore.Revision
+}
+
+func (rwt *natsReadWriteTx) WriteRelationships(_ context.Context, mutations []tuple.RelationshipUpdate) error {
+	rwt.mustLock()
+	defer rwt.Unlock()
+
+	tx, err := rwt.txSource()
+	if err != nil {
+		return err
+	}
+
+	return rwt.write(tx, mutations...)
+}
+
+func (rwt *natsReadWriteTx) toIntegrity(mutation tuple.RelationshipUpdate) *relationshipIntegrity {
+	var ri *relationshipIntegrity
+	if mutation.Relationship.OptionalIntegrity != nil {
+		ri = &relationshipIntegrity{
+			keyID:     mutation.Relationship.OptionalIntegrity.KeyId,
+			hash:      mutation.Relationship.OptionalIntegrity.Hash,
+			timestamp: mutation.Relationship.OptionalIntegrity.HashedAt.AsTime(),
+		}
+	}
+	return ri
+}
+
+// Caller must already hold the concurrent access lock!
+func (rwt *natsReadWriteTx) write(tx *memdb.Txn, mutations ...tuple.RelationshipUpdate) error {
+	// Apply the mutations
+	for _, mutation := range mutations {
+		rel := &relationship{
+			mutation.Relationship.Resource.ObjectType,
+			mutation.Relationship.Resource.ObjectID,
+			mutation.Relationship.Resource.Relation,
+			mutation.Relationship.Subject.ObjectType,
+			mutation.Relationship.Subject.ObjectID,
+			mutation.Relationship.Subject.Relation,
+			rwt.toCaveatReference(mutation),
+			rwt.toIntegrity(mutation),
+			mutation.Relationship.OptionalExpiration,
+		}
+
+		found, err := tx.First(
+			tableRelationship,
+			indexID,
+			rel.namespace,
+			rel.resourceID,
+			rel.relation,
+			rel.subjectNamespace,
+			rel.subjectObjectID,
+			rel.subjectRelation,
+		)
+		if err != nil {
+			return fmt.Errorf("error loading existing relationship: %w", err)
+		}
+
+		var existing *relationship
+		if found != nil {
+			existing = found.(*relationship)
+		}
+
+		switch mutation.Operation {
+		case tuple.UpdateOperationCreate:
+			if existing != nil {
+				rt, err := existing.Relationship()
+				if err != nil {
+					return err
+				}
+				return common.NewCreateRelationshipExistsError(&rt)
+			}
+			if err := tx.Insert(tableRelationship, rel); err != nil {
+				return fmt.Errorf("error inserting relationship: %w", err)
+			}
+
+		case tuple.UpdateOperationTouch:
+			if existing != nil {
+				rt, err := existing.Relationship()
+				if err != nil {
+					return err
+				}
+				if tuple.MustString(rt) == tuple.MustString(mutation.Relationship) {
+					continue
+				}
+			}
+
+			if err := tx.Insert(tableRelationship, rel); err != nil {
+				return fmt.Errorf("error inserting relationship: %w", err)
+			}
+
+		case tuple.UpdateOperationDelete:
+			if existing != nil {
+				if err := tx.Delete(tableRelationship, existing); err != nil {
+					return fmt.Errorf("error deleting relationship: %w", err)
+				}
+			}
+		default:
+			return spiceerrors.MustBugf("unknown tuple mutation operation type: %v", mutation.Operation)
+		}
+	}
+
+	return nil
+}
+
+func (rwt *natsReadWriteTx) toCaveatReference(mutation tuple.RelationshipUpdate) *contextualizedCaveat {
+	var cr *contextualizedCaveat
+	if mutation.Relationship.OptionalCaveat != nil {
+		cr = &contextualizedCaveat{
+			caveatName: mutation.Relationship.OptionalCaveat.CaveatName,
+			context:    mutation.Relationship.OptionalCaveat.Context.AsMap(),
+		}
+	}
+	return cr
+}
+
+func (rwt *natsReadWriteTx) DeleteRelationships(_ context.Context, filter *v1.RelationshipFilter, opts ...options.DeleteOptionsOption) (bool, error) {
+	rwt.mustLock()
+	defer rwt.Unlock()
+
+	tx, err := rwt.txSource()
+	if err != nil {
+		return false, err
+	}
+
+	delOpts := options.NewDeleteOptionsWithOptionsAndDefaults(opts...)
+	var delLimit uint64
+	if delOpts.DeleteLimit != nil && *delOpts.DeleteLimit > 0 {
+		delLimit = *delOpts.DeleteLimit
+	}
+
+	return rwt.deleteWithLock(tx, filter, delLimit)
+}
+
+// caller must already hold the concurrent access lock
+func (rwt *natsReadWriteTx) deleteWithLock(tx *memdb.Txn, filter *v1.RelationshipFilter, limit uint64) (bool, error) {
+	// Create an iterator to find the relevant tuples
+	dsFilter, err := datastore.RelationshipsFilterFromPublicFilter(filter)
+	if err != nil {
+		return false, err
+	}
+
+	bestIter, err := iteratorForFilter(tx, dsFilter)
+	if err != nil {
+		return false, err
+	}
+	filteredIter := memdb.NewFilterIterator(bestIter, relationshipFilterFilterFunc(filter))
+
+	// Collect the tuples into a slice of mutations for the changelog
+	var mutations []tuple.RelationshipUpdate
+	var counter uint64
+
+	metLimit := false
+	for row := filteredIter.Next(); row != nil; row = filteredIter.Next() {
+		rt, err := row.(*relationship).Relationship()
+		if err != nil {
+			return false, err
+		}
+		mutations = append(mutations, tuple.Delete(rt))
+		counter++
+
+		if limit > 0 && counter == limit {
+			metLimit = true
+			break
+		}
+	}
+
+	return metLimit, rwt.write(tx, mutations...)
+}
+
+func (rwt *natsReadWriteTx) RegisterCounter(ctx context.Context, name string, filter *core.RelationshipFilter) error {
+	rwt.mustLock()
+	defer rwt.Unlock()
+
+	tx, err := rwt.txSource()
+	if err != nil {
+		return err
+	}
+
+	foundRaw, err := tx.First(tableCounters, indexID, name)
+	if err != nil {
+		return err
+	}
+
+	if foundRaw != nil {
+		return datastore.NewCounterAlreadyRegisteredErr(name, filter)
+	}
+
+	filterBytes, err := filter.MarshalVT()
+	if err != nil {
+		return err
+	}
+
+	// Insert the counter
+	counter := &counter{
+		name,
+		filterBytes,
+		0,
+		datastore.NoRevision,
+	}
+
+	return tx.Insert(tableCounters, counter)
+}
+
+func (rwt *natsReadWriteTx) UnregisterCounter(ctx context.Context, name string) error {
+	rwt.mustLock()
+	defer rwt.Unlock()
+
+	tx, err := rwt.txSource()
+	if err != nil {
+		return err
+	}
+
+	// Check if the counter exists
+	foundRaw, err := tx.First(tableCounters, indexID, name)
+	if err != nil {
+		return err
+	}
+
+	if foundRaw == nil {
+		return datastore.NewCounterNotRegisteredErr(name)
+	}
+
+	return tx.Delete(tableCounters, foundRaw)
+}
+
+func (rwt *natsReadWriteTx) StoreCounterValue(ctx context.Context, name string, value int, computedAtRevision datastore.Revision) error {
+	rwt.mustLock()
+	defer rwt.Unlock()
+
+	tx, err := rwt.txSource()
+	if err != nil {
+		return err
+	}
+
+	// Check if the counter exists
+	foundRaw, err := tx.First(tableCounters, indexID, name)
+	if err != nil {
+		return err
+	}
+
+	if foundRaw == nil {
+		return datastore.NewCounterNotRegisteredErr(name)
+	}
+
+	counter := foundRaw.(*counter)
+	counter.count = value
+	counter.updated = computedAtRevision
+
+	return tx.Insert(tableCounters, counter)
+}
+
+func (rwt *natsReadWriteTx) WriteNamespaces(_ context.Context, newConfigs ...*core.NamespaceDefinition) error {
+	rwt.mustLock()
+	defer rwt.Unlock()
+
+	tx, err := rwt.txSource()
+	if err != nil {
+		return err
+	}
+
+	for _, newConfig := range newConfigs {
+		serialized, err := newConfig.MarshalVT()
+		if err != nil {
+			return err
+		}
+
+		newConfigEntry := &namespace{newConfig.Name, serialized, rwt.newRevision}
+
+		err = tx.Insert(tableNamespace, newConfigEntry)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (rwt *natsReadWriteTx) DeleteNamespaces(_ context.Context, nsNames ...string) error {
+	rwt.mustLock()
+	defer rwt.Unlock()
+
+	tx, err := rwt.txSource()
+	if err != nil {
+		return err
+	}
+
+	for _, nsName := range nsNames {
+		foundRaw, err := tx.First(tableNamespace, indexID, nsName)
+		if err != nil {
+			return err
+		}
+
+		if foundRaw == nil {
+			return fmt.Errorf("namespace not found")
+		}
+
+		if err := tx.Delete(tableNamespace, foundRaw); err != nil {
+			return err
+		}
+
+		// Delete the relationships from the namespace
+		if _, err := rwt.deleteWithLock(tx, &v1.RelationshipFilter{
+			ResourceType: nsName,
+		}, 0); err != nil {
+			return fmt.Errorf("unable to delete relationships from deleted namespace: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (rwt *natsReadWriteTx) BulkLoad(ctx context.Context, iter datastore.BulkWriteRelationshipSource) (uint64, error) {
+	var numCopied uint64
+	var next *tuple.Relationship
+	var err error
+
+	updates := []tuple.RelationshipUpdate{{
+		Operation: tuple.UpdateOperationCreate,
+	}}
+
+	for next, err = iter.Next(ctx); next != nil && err == nil; next, err = iter.Next(ctx) {
+		updates[0].Relationship = *next
+		if err := rwt.WriteRelationships(ctx, updates); err != nil {
+			return 0, err
+		}
+		numCopied++
+	}
+
+	return numCopied, err
+}
+
+func relationshipFilterFilterFunc(filter *v1.RelationshipFilter) func(interface{}) bool {
+	return func(tupleRaw interface{}) bool {
+		tuple := tupleRaw.(*relationship)
+
+		// If it doesn't match one of the resource filters, filter it.
+		switch {
+		case filter.ResourceType != "" && filter.ResourceType != tuple.namespace:
+			return true
+		case filter.OptionalResourceId != "" && filter.OptionalResourceId != tuple.resourceID:
+			return true
+		case filter.OptionalResourceIdPrefix != "" && !strings.HasPrefix(tuple.resourceID, filter.OptionalResourceIdPrefix):
+			return true
+		case filter.OptionalRelation != "" && filter.OptionalRelation != tuple.relation:
+			return true
+		}
+
+		// If it doesn't match one of the subject filters, filter it.
+		if subjectFilter := filter.OptionalSubjectFilter; subjectFilter != nil {
+			switch {
+			case subjectFilter.SubjectType != tuple.subjectNamespace:
+				return true
+			case subjectFilter.OptionalSubjectId != "" && subjectFilter.OptionalSubjectId != tuple.subjectObjectID:
+				return true
+			case subjectFilter.OptionalRelation != nil &&
+				stringz.DefaultEmpty(subjectFilter.OptionalRelation.Relation, datastore.Ellipsis) != tuple.subjectRelation:
+				return true
+			}
+		}
+
+		return false
+	}
+}
+
+var _ datastore.ReadWriteTransaction = &natsReadWriteTx{}
